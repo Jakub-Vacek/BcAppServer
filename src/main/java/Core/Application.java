815cadd00d6389a7e2a05948d7a294e461237800
Jakub-Vacek <vacjak@seznam.cz>
2017-04-03 16:31:49 +0200

    Add mock data initialisation

--- src/main/java/Core/Application.java
+++ src/main/java/Core/Application.java
@@ -1,6 +1,6 @@
 package Core;
 
-//import Mock.MockDataInitializer;
+import Mock.MockDataInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
